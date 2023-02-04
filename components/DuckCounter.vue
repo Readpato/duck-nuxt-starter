bdd3348fff475d3a29aaf2b3c72acb331a45d14f
Patrick Raedler <patoraedler@gmail.com>
2023-02-04 18:23:27 +0100

    feat(TheCounter): create the counter component

---
+++ components/DuckCounter.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div
+    aria-label="counter"
+    class="flex justify-center items-center space-x-3 mt-2 text-center"
+  >
+    <button aria-label="increment-counter" class="w-8 h-8 border border-green border-2 rounded hover:bg-white hover:text-black transition-all ease-in-out duration-400" @click="count--">
+      -
+    </button>
+    <span aria-label="counter-value" class="min-w-5">{{ count }}</span>
+    <button
+      aria-label="decrement-counter"
+      class="w-8 h-8 border border-green border-2 rounded hover:bg-white hover:text-black transition-all ease-in-out duration-400" @click="count++"
+    >
+      +
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- components/DuckWelcome.vue
+++ components/DuckWelcome.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+const title = 'Duck nuxt starter'
 </script>
 
 <template>
   <main class="w-screen h-screen bg-black flex flex-col justify-center items-center text-white">
     <img src="@/assets/nuxt-logo.svg" alt="Nuxt logo">
     <h1 class="font-light text-2xl mt-2">
-      Duck nuxt starter
+      {{ title }}
     </h1>
+    <DuckCounter />
   </main>
 </template>
